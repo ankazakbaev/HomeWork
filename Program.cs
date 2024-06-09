316b1d4ed49802cb306ec7ed24d2a9829e9ed9f9
ankazakbaev <kazakbaevazat@mail.ru>
2024-06-09 15:55:55 +0300

    4
    4

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("Введите первое число: ");
+string str1 = Console.ReadLine();
+int ask = Convert.ToInt32(str1);
+Console.WriteLine("Введите второе число: ");
+string str2 = Console.ReadLine();
+int ask2 = Convert.ToInt32(str2);
+if (ask > ask2)
+{
+    Console.WriteLine (ask);
+}
+else
+Console.WriteLine ("Наибольшее число: ");
+Console.WriteLine (ask2);
